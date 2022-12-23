3668e135a0373221d8ca358d98abb62bf9fcaf1b
SACHIN DHANKHAR <114800115+Sachindhankhar2003@users.noreply.github.com>
2022-12-23 23:02:03 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,14 @@
+#store multiple value in single variable
+#unordered
+# set are unchangeable
+#duplicate are not allowed in set
+#
+
+
+mySet = {"car","bike","boat","train"}
+mySet1 = {1,2,3,4}
+mySet2  = {4,5,6,}
+ #mySet3 = mySet1.union(mySet2)
+mySet3 = mySet1.intersection(mySet2)
+mySet4 = mySet1.symmetric_difference(mySet2)
+print(mySet4)
